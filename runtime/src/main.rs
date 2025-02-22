fa6c9706f3d17d85bc79edb87de9d2d15886fcfb
李兴栋 <lixingdong@aliyun.com>
2025-02-22 11:18:35 +0800

    ...

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -70,6 +70,7 @@ async fn test_export_zip() {
     tr.transaction(MyCommand::new()).await;
     let _ = runtime.dispatch(tr).await;
     runtime.export_zip(current_dir().unwrap().join("test.zip").as_path()).await;
+    tokio::time::sleep(std::time::Duration::from_secs(50)).await;
 }
 fn get_base() -> Vec<Extensions> {
     let mut extensions = vec![];
