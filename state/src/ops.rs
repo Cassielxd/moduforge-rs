593a329eef89ccfbee9cad1c1face1294dd5b896
李兴栋 <lixingdong@aliyun.com>
2025-04-14 17:43:39 +0800

    ...

--- state/src/ops.rs
+++ state/src/ops.rs
@@ -5,27 +5,39 @@ use crate::{
     resource_table::ResourceTable,
 };
 
+/// 全局资源管理器
+/// 用于管理编辑器运行时的全局资源和状态
+/// - resource_table: 资源表，管理所有注册的资源
+/// - gotham_state: Gotham状态，管理特定于Gotham框架的状态
 #[derive(Default, Debug)]
 pub struct GlobalResourceManager {
     pub resource_table: ResourceTable,
     pub gotham_state: GothamState
 }
+
+// 实现Send和Sync trait，表明GlobalResourceManager可以在线程间安全传递和共享
 unsafe impl Send for GlobalResourceManager {}
 unsafe impl Sync for GlobalResourceManager {}
+
 impl GlobalResourceManager {
+    /// 创建新的全局资源管理器实例
     pub fn new() -> Self {
         Self {
             resource_table: ResourceTable::default(),
             gotham_state: GothamState::default()
         }
     }
+
+    /// 清理所有资源
+    /// 使用std::mem::take来安全地获取并重置内部状态
     pub fn clear(&mut self) {
         std::mem::take(&mut self.gotham_state);
         std::mem::take(&mut self.resource_table);
     }
-
 }
 
+// 实现Deref trait，允许将GlobalResourceManager当作GothamState使用
+// 这样可以直接访问gotham_state的方法和字段
 impl Deref for GlobalResourceManager {
     type Target = GothamState;
 
@@ -34,6 +46,7 @@ impl Deref for GlobalResourceManager {
     }
 }
 
+// 实现DerefMut trait，允许可变地访问gotham_state
 impl DerefMut for GlobalResourceManager {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.gotham_state
