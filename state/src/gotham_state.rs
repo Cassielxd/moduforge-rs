b079ea6210d6ec8b7d9a4fedaa70a5723689dab9
李兴栋 <lixingdong@aliyun.com>
2025-04-30 11:25:52 +0800

    优化op_state

--- state/src/gotham_state.rs
+++ state/src/gotham_state.rs
@@ -1,23 +1,24 @@
 use std::any::Any;
 use std::any::TypeId;
 use std::any::type_name;
-use std::collections::BTreeMap;
+
+use dashmap::DashMap;
+
 
 
 
 #[derive(Default, Debug)]
 pub struct GothamState {
     /// 使用BTreeMap存储不同类型的数据，以TypeId为键
-    data: BTreeMap<TypeId, Box<dyn Any>>,
+    data: DashMap<TypeId, Box<dyn Any>>,
 }
 
-impl GothamState {
-    /// 将数据存入状态容器中
+impl GothamState {  /// 将数据存入状态容器中
     ///
     /// # 参数
     /// * `t` - 要存储的数据，必须是'static生命周期
-    pub fn put<T: 'static>(
-        &mut self,
+    pub fn put<T: Clone+ 'static>(
+        &self,
         t: T,
     ) {
         let type_id = TypeId::of::<T>();
@@ -35,65 +36,36 @@ impl GothamState {
         let type_id = TypeId::of::<T>();
         self.data.contains_key(&type_id)
     }
-
-    /// 尝试获取状态容器中指定类型数据的不可变引用
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要获取的类型
-    ///
-    /// # 返回值
-    /// * 如果存在返回Some(&T)，否则返回None
-    pub fn try_borrow<T: 'static>(&self) -> Option<&T> {
+    pub fn get<T: Clone+'static>(&self) -> T {
+        self.try_get::<T>().unwrap_or_else(|| missing::<T>())
+    }
+    pub fn try_get<T:Clone+ 'static>(&self) -> Option<T> {
         let type_id = TypeId::of::<T>();
-        self.data.get(&type_id).and_then(|b| b.downcast_ref())
+        let data = self.data.get(&type_id);
+        match data {
+         Some(v) => {
+             let value = v.value().as_ref().downcast_ref::<T>();
+             value.cloned()
+         },
+         None => None,
+        }
     }
 
-    /// 获取状态容器中指定类型数据的不可变引用
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要获取的类型
-    ///
-    /// # 返回值
-    /// * 返回&T，如果数据不存在则panic
-    pub fn borrow<T: 'static>(&self) -> &T {
-        self.try_borrow().unwrap_or_else(|| missing::<T>())
-    }
-
-    /// 尝试获取状态容器中指定类型数据的可变引用
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要获取的类型
-    ///
-    /// # 返回值
-    /// * 如果存在返回Some(&mut T)，否则返回None
-    pub fn try_borrow_mut<T: 'static>(&mut self) -> Option<&mut T> {
-        let type_id = TypeId::of::<T>();
-        self.data.get_mut(&type_id).and_then(|b| b.downcast_mut())
-    }
-
-    /// 获取状态容器中指定类型数据的可变引用
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要获取的类型
-    ///
-    /// # 返回值
-    /// * 返回&mut T，如果数据不存在则panic
-    pub fn borrow_mut<T: 'static>(&mut self) -> &mut T {
-        self.try_borrow_mut().unwrap_or_else(|| missing::<T>())
-    }
-
-    /// 尝试从状态容器中移除并返回指定类型的数据
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要移除的类型
-    ///
-    /// # 返回值
-    /// * 如果存在返回Some(T)，否则返回None
     pub fn try_take<T: 'static>(&mut self) -> Option<T> {
         let type_id = TypeId::of::<T>();
-        self.data.remove(&type_id).and_then(|b| b.downcast().ok()).map(|b| *b)
+        match self.data.remove(&type_id) {
+            Some((_, v)) => {
+                let value = v.downcast::<T>();
+                match value {
+                    Ok(v) => Some(*v),
+                    Err(e) => {
+                        None
+                    }
+                }
+            }
+            None => None,
+        }
     }
-
     /// 从状态容器中移除并返回指定类型的数据
     ///
     /// # 参数
@@ -115,7 +87,7 @@ impl GothamState {
 /// * 永不返回，总是panic
 fn missing<T: 'static>() -> ! {
     panic!(
-        "required type {} is not present in GothamState container",
+        " 请求的类型 {} 不存在于 GothamState 容器中",
         type_name::<T>()
     );
 }
--- state/src/ops.rs
+++ state/src/ops.rs
@@ -1,8 +1,7 @@
-use std::{any::{type_name, Any, TypeId}, ops::{Deref, DerefMut}, sync::Arc};
+use std::ops::{Deref, DerefMut};
 
-use dashmap::DashMap;
 
-use crate::resource_table::ResourceTable;
+use crate::{gotham_state::GothamState, resource_table::ResourceTable};
 
 /// 全局资源管理器
 /// 用于管理编辑器运行时的全局资源和状态
@@ -11,7 +10,7 @@ use crate::resource_table::ResourceTable;
 #[derive(Default, Debug)]
 pub struct GlobalResourceManager {
     pub resource_table: ResourceTable,
-    pub gotham_state: DashMap<TypeId, Box<dyn Any>>,
+    pub gotham_state: GothamState,
 }
 
 // 实现Send和Sync trait，表明GlobalResourceManager可以在线程间安全传递和共享
@@ -23,7 +22,7 @@ impl GlobalResourceManager {
     pub fn new() -> Self {
         Self {
             resource_table: ResourceTable::default(),
-            gotham_state: DashMap::new(),
+            gotham_state: GothamState::default(),
         }
     }
 
@@ -33,73 +32,18 @@ impl GlobalResourceManager {
         std::mem::take(&mut self.gotham_state);
         std::mem::take(&mut self.resource_table);
     }    
-    /// 将数据存入状态容器中
-    ///
-    /// # 参数
-    /// * `t` - 要存储的数据，必须是'static生命周期
-    pub fn put<T: Clone+ 'static>(
-        &self,
-        t: T,
-    ) {
-        let type_id = TypeId::of::<T>();
-        self.gotham_state.insert(type_id, Box::new(t));
-    }
+  
+}
+impl Deref for GlobalResourceManager {
+    type Target = GothamState;
 
-    /// 检查状态容器中是否包含指定类型的数据
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要检查的类型
-    ///
-    /// # 返回值
-    /// * 如果存在返回true，否则返回false
-    pub fn has<T: 'static>(&self) -> bool {
-        let type_id = TypeId::of::<T>();
-        self.gotham_state.contains_key(&type_id)
-    }
-    pub fn get<T: Clone+'static>(&self) -> T {
-        self.try_get::<T>().unwrap_or_else(|| missing::<T>())
-    }
-    pub fn try_get<T:Clone+ 'static>(&self) -> Option<T> {
-        let type_id = TypeId::of::<T>();
-        let data = self.gotham_state.get(&type_id);
-        match data {
-         Some(v) => {
-             let value = v.value().as_ref().downcast_ref::<T>();
-             value.cloned()
-         },
-         None => None,
-        }
-    }
-
-    pub fn try_take<T: 'static>(&mut self) -> Option<T> {
-        let type_id = TypeId::of::<T>();
-        match self.gotham_state.remove(&type_id) {
-            Some((_, v)) => {
-                let value = v.downcast::<T>();
-                match value {
-                    Ok(v) => Some(*v),
-                    Err(e) => {
-                        None
-                    }
-                }
-            }
-            None => None,
-        }
-    }
-    /// 从状态容器中移除并返回指定类型的数据
-    ///
-    /// # 参数
-    /// * 泛型参数T - 要移除的类型
-    ///
-    /// # 返回值
-    /// * 返回T，如果数据不存在则panic
-    pub fn take<T: 'static>(&mut self) -> T {
-        self.try_take().unwrap_or_else(|| missing::<T>())
+    fn deref(&self) -> &Self::Target {
+        &self.gotham_state
     }
 }
-fn missing<T: 'static>() -> ! {
-    panic!(
-        "required type {} is not present in GothamState container",
-        type_name::<T>()
-    );
+
+impl DerefMut for GlobalResourceManager {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.gotham_state
+    }
 }
