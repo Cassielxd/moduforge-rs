2b1bea2b7bbf23eebf456bef10e3fea8361b7def
李兴栋 <lixingdong@aliyun.com>
2025-07-29 16:16:38 +0800

    。。。

--- crates/derive/src/lib.rs
+++ crates/derive/src/lib.rs
@@ -1,11 +1,5 @@
 //! ModuForge 派生宏
 //!
-//! 该模块提供了ModuForge项目的所有派生宏，包括：
-//! - PState: 插件状态派生宏
-//! - Component: 依赖注入组件派生宏
-//! - Injectable: 可注入字段派生宏
-//! - service: 服务标记宏
-//! - bean: Bean方法标记宏
 
 use proc_macro::TokenStream;
 use quote::quote;
